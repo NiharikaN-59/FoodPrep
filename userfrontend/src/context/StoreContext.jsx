236c86a6bad45823613896221823f4ccededf8d8
Niharika <niharikaneelam2006@gmail.com>
2025-07-02 14:33:31 +0530

    updated urls

--- userfrontend/src/context/StoreContext.jsx
+++ userfrontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext();
 const StoreContextProvider = ({children})=>{
     const [cartItems,setCartItems] = useState({})
     const [food_list,setFoodList] = useState([])
-    const url = 'http://localhost:4000'
+    const url = 'https://foodprep-ar0u.onrender.com'
     const [token,setToken] = useState("")
 
     const fetchFoodList = async()=>{
